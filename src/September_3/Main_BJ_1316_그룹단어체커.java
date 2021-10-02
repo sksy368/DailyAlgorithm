4da3446fb96cc4091ae77e8f148fd6752f1a4a85
roseline <roseline@218.38.137.27>
2021-10-03 01:50:57 +0900

    https://www.acmicpc.net/problem/1316

---
+++ src/September_3/Main_BJ_1316_그룹단어체커.java
@@ -0,0 +1,31 @@
+package September_3;
+
+import java.util.*;
+
+public class Main_BJ_1316_그룹단어체커 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int cnt = 0;
+		
+		for(int n = 0; n < N; n++) {
+			String word = sc.next()+'?';
+			boolean[] check = new boolean[26];
+			
+			cnt++;
+			for(int i = 0; i < word.length()-1; i++) {
+				if(word.charAt(i) == word.charAt(i+1)) continue;
+				else {
+					if(check[word.charAt(i) - 'a']) {
+						cnt--;
+						break;
+					}
+					check[word.charAt(i) - 'a'] = true;
+				}
+			}
+		}
+		System.out.println(cnt);
+	}
+}
