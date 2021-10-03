0f0178e9039b72d7f3a116d699ccb4002559ba15
roseline <75062273+sksy368@users.noreply.github.com>
2021-10-03 12:25:04 +0900

    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PpLlKAQ4DFAUq

---
+++ src/Graph/com/ssafy/Practice_0930/Solution_SWEA_1953_탈주범검거.java
@@ -0,0 +1,12 @@
+package Graph.com.ssafy.Practice_0930;
+
+import java.util.*;
+import java.io.*;
+
+public class Solution_SWEA_1953_탈주범검거 {
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
