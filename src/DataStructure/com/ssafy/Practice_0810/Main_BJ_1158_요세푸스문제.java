074dd3b59e195620d112111e207682cf83cb1097
roseline <75062273+sksy368@users.noreply.github.com>
2021-10-03 11:49:07 +0900

    https://www.acmicpc.net/problem/1158

---
+++ src/DataStructure/com/ssafy/Practice_0810/Main_BJ_1158_요세푸스문제.java
@@ -0,0 +1,39 @@
+package DataStructure.com.ssafy.Practice_0810;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main_BJ_1158_요세푸스문제 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		Queue<Integer> queue = new LinkedList<Integer>();
+		
+		int N = scanner.nextInt();
+		int K = scanner.nextInt();
+		
+		for(int i = 1; i <= N; i++)
+			queue.add(i);
+		
+		int check = 1;
+		System.out.print("<");
+		
+		while(!queue.isEmpty()) {
+			if(check == K) {
+				System.out.print(queue.poll());
+				if(!queue.isEmpty())
+					System.out.print(", ");
+				
+				check = 1;
+				continue;
+			}
+			
+			queue.add(queue.poll());
+			check += 1;
+		}
+		
+		System.out.print(">");
+	}
+}
