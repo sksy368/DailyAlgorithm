90f93a08bc6f2b80aa91ea6eeac4163a4e434db6
roseline <75062273+sksy368@users.noreply.github.com>
2021-10-19 00:59:19 +0900

    https://www.acmicpc.net/problem/1182

---
+++ src/Graph/roseline/Main_BJ_1240_노드사이의거리.java
@@ -0,0 +1,15 @@
+package Graph.roseline;
+
+import java.util.*;
+import java.io.*;
+
+public class Main_BJ_1240_노드사이의거리 {
+	
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+	}
+}
