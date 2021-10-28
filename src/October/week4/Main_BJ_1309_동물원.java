095b68d8b007ddaa887d3d463b7a00697c65c23f
roseline <75062273+sksy368@users.noreply.github.com>
2021-10-28 20:25:43 +0900

    https://www.acmicpc.net/problem/1309

---
+++ src/October/week4/Main_BJ_1309_동물원.java
@@ -0,0 +1,21 @@
+package October.week4;
+
+import java.io.*;
+
+public class Main_BJ_1309_동물원 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] D = new int[N+1];
+		D[0] = 1;
+		D[1] = 3;
+		
+		for(int i = 2; i <= N; i++)
+			D[i] = (D[i-2] + D[i-1]*2) % 9901;
+		
+		System.out.println(D[N]);
+	}
+}
