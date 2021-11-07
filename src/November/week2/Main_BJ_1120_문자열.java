dc15ade4ff028919817768255a6e0d6eebc8f5ef
roseline <75062273+sksy368@users.noreply.github.com>
2021-11-07 14:11:03 +0900

    https://www.acmicpc.net/problem/1120

---
+++ src/November/week2/Main_BJ_1120_문자열.java
@@ -0,0 +1,32 @@
+package November.week2;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_BJ_1120_문자열 {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String word1 = st.nextToken();
+		String word2 = st.nextToken() + " ";
+		
+		int num = word2.length() - word1.length(); // 비교 횟수 = 두 문자열 길이의 차
+		int min = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < num; i++) {
+			String temp = word2.substring(i, i+word1.length());
+			
+			int sum = 0;
+			for(int j = 0; j < word1.length(); j++) {
+				if(word1.charAt(j) != temp.charAt(j))
+					sum++;
+			}
+			
+			min = Math.min(sum, min);
+		}
+		
+		System.out.println(min);
+	}
+}
