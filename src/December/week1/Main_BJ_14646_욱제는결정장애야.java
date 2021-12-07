0a3cbede2ef01f0c4f9e67f942790087888e307b
roseline <75062273+sksy368@users.noreply.github.com>
2021-12-07 20:49:11 +0900

    https://www.acmicpc.net/problem/14646

---
+++ src/December/week1/Main_BJ_14646_욱제는결정장애야.java
@@ -0,0 +1,33 @@
+package December.week1;
+
+import java.util.*;
+import java.io.*;
+
+public class Main_BJ_14646_욱제는결정장애야 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine()); // 메뉴의 개수
+		int[] board = new int[N+1]; // 돌림판
+		int cnt = 0;
+		int max = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int n = 0; n < 2*N; n++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			if(board[num] == 0) {
+				board[num] = 1;
+				cnt++;
+			}
+			else
+				cnt--;
+			
+			max = Math.max(max, cnt);
+		}
+		
+		System.out.println(max);
+	}
+}
