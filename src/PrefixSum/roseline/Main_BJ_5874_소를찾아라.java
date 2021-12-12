1cc6c72607e78981ee3e536c5fc1039f6ce47908
roseline <75062273+sksy368@users.noreply.github.com>
2021-12-12 13:01:04 +0900

    https://www.acmicpc.net/problem/5874

---
+++ src/PrefixSum/roseline/Main_BJ_5874_소를찾아라.java
@@ -0,0 +1,24 @@
+package PrefixSum.roseline;
+
+import java.io.*;
+
+public class Main_BJ_5874_소를찾아라 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String grass = br.readLine();
+	
+		int cnt = 0;
+		int hindLegs = 0; // 뒷다리 개수
+		
+		for(int i = 0; i < grass.length()-1; i++) {
+			if(grass.charAt(i) == '(' && grass.charAt(i+1) == '(') // 뒷다리인 경우
+				hindLegs++;
+			if(grass.charAt(i) == ')' && grass.charAt(i+1) == ')') // 앞다리인 경우
+				cnt += hindLegs;
+		}
+		
+		System.out.println(cnt);
+	}
+}
