a26177b4672e91da5da74248a22546f933fc81ba
roseline <sksy368@naver.com>
2022-03-07 16:30:09 +0900

    Chore: 주석 추가

--- src/Mar_2022/week1/Solution_Programmers_60057_문자열압축.java
+++ src/Mar_2022/week1/Solution_Programmers_60057_문자열압축.java
@@ -16,14 +16,14 @@ public class Solution_Programmers_60057_문자열압축 {
 		for(int i = 1; i <= input.length()/2; i++) { // i:자르는 단위 개수
 			String output = "";
 			String before = "";
-			int degree = 1;
+			int degree = 1; // 동일한 문자열이 반복되는 횟수
 			
 			for(int j = 0; j < input.length()/i; j++) { // j : offset
 				String now = input.substring(i*j, i*(j+1));
 				
-				if(now.equals(before))
+				if(now.equals(before)) // 이전 문자열과 같은 경우
 					degree++;
-				else {
+				else { // 이전 문자열과 다른 경우
 					if(degree != 1) output += degree;
 					output += before;
 					degree = 1;
@@ -31,9 +31,8 @@ public class Solution_Programmers_60057_문자열압축 {
 				before = now;
 			}
 			
-			
-			output += degree!=1 ? degree+before: before;
-			if(input.length()%i != 0) output += input.substring(input.length()/i*i);
+			output += degree!=1 ? degree+before: before; // 마지막 문자열
+			if(input.length()%i != 0) output += input.substring(input.length()/i*i); // 문자열이 남을 경우
 			minLen = Math.min(output.length(), minLen);
 		}
 		
