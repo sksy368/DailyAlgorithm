a9dae12588a16ed65146e832617b5783c844aa47
roseline <sksy368@naver.com>
2022-03-25 03:16:18 +0900

    Code: Solve boj 2002 추월
    Ref: #구현, #문자열

--- src/Mar_2022/week4/Main_BJ_2002_추월.java
+++ src/Mar_2022/week4/Main_BJ_2002_추월.java
@@ -1,48 +1,48 @@
 package Mar_2022.week4;
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main_BJ_2002_추월 {
 	
 	static class Car {
-		String no; // 차 번호
-		HashSet<String> front = new HashSet<>(); // 이전의 차들 번호
-		Car(String no, HashSet<String> front){
+		String no;
+		HashSet<String> set;
+		Car(String no, HashSet<String> set){
 			this.no = no;
-			this.front = front;
+			this.set = set;
 		}
 	}
-
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine()); // 차의 대수
 		int cnt = 0; // 추월한 차 대수
 		
-		Car[] cars = new Car[N];
-		HashSet<String> set = new HashSet<>();
+		Car[] input = new Car[N];
+		String[] output = new String[N];
 		
-		for(int n = 0; n < N; n++) {
+		for(int i = 0; i < N; i++) {
 			String car = br.readLine();
-			cars[n] = new Car(car, set);
-			set.add(car);
-			System.out.println(cars[n].front.size());
+			HashSet<String> set = new HashSet<>();
+			
+			for(int j = 0; j < i; j++) set.add(input[j].no);
+			
+			input[i] = new Car(car, set);
 		}
 		
-		for(int n = 0; n < N; n++) {
-			String car = br.readLine();
-			if(cars[n].no.equals(car)) { // 이전의 순서와 같은 경우
-				cnt++;
-				continue;
-			}
+		for(int n = 0; n < N; n++) output[n] = br.readLine();
+		
+		for(int i = 0; i < N; i++) {
+			int idx = -1;
 			
-			HashSet<String> front = cars[n].front;
-			int beforeSize = front.size();
-			front.add(car);
-			int afterSize = front.size();
+			for(int j = 0; j < N; j++)
+				if(output[i].equals(input[j].no)) idx = j;
 			
-			if(beforeSize < afterSize) cnt++;
+			for(int j = 0; j < i; j++) input[idx].set.remove(output[j]);
+			
+			if(input[idx].set.size() != 0) cnt++;
 		}
 		
 		System.out.println(cnt);
