b9bf5ebbea1d13691347abd529d6bea42f37f26c
roseline <sksy368@naver.com>
2022-04-03 18:01:01 +0900

    Code: Solve dfs

--- src/Mar_2022/week5/Main_BJ_43164_여행경로.java
+++ src/Mar_2022/week5/Main_BJ_43164_여행경로.java
@@ -2,40 +2,34 @@ package Mar_2022.week5;
 
 import java.util.*;
 
-class Solution {
+class Main_BJ_43164_여행경로 {
+	
+	ArrayList<String> list = new ArrayList<>();
+	PriorityQueue<String> queue = new PriorityQueue<>();
+	boolean[] checked;
+	
     public String[] solution(String[][] tickets) {
-        HashMap<String, PriorityQueue<String>> map = new HashMap<>();
+    	checked = new boolean[tickets.length];
+        dfs("ICN", 0, tickets);
         
-        for(int i = 0; i < tickets.length; i++) {
-        	String start = tickets[i][0];
-        	String end = tickets[i][1];
-        	PriorityQueue<String> queue;
-        	
-        	if(map.containsKey(start)) queue = map.get(start);
-        	else queue = new PriorityQueue<>();	
-        	
-        	queue.add(end);
-        	map.put(start, queue);
-        }
-        
-        ArrayList<String> list = new ArrayList<>();
-        list.add("ICN");
-        while(true) {
-            String next;
-        	try {
-                 next = map.get(list.get(list.size()-1)).poll();
-            } catch (NullPointerException e) {
-                break;
-            }
-            
-            list.add(next);
-        }
-        
-        if(list.get(list.size()-1) == null) list.remove(list.size()-1);
-        
-        String[] answer = new String[list.size()];
-        for(int i = 0; i < list.size(); i++) answer[i] = list.get(i);
-        
-        return answer;
+        return queue.poll().split(" ");
+    }
+    
+    public void dfs(String desc, int len, String[][] tickets) {
+    	list.add(desc);
+    	
+    	if(len == tickets.length) { // 모든 티켓을 사용한 경우
+    		queue.offer(String.join(" ", list));
+    		return;
+    	}
+    	
+    	for(int i = 0; i < tickets.length; i++) {
+    		if(!checked[i] && tickets[i][0].equals(desc)) {// 티켓을 사용하지 않았고, 해당 도착지(desc)가 티켓의 출발지와 같은 경우
+    			checked[i] = true;
+    			dfs(tickets[i][1], len+1, tickets);
+    			checked[i] = false;
+    			list.remove(list.size()-1);
+    		}
+    	}
     }
 }
