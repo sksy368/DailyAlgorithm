122874e5e96824b989beea1690765a4cac94d7e6
roseline <sksy368@naver.com>
2022-04-17 14:17:08 +0900

    Code : Solve with using variable

--- src/Apr_2022/week2/Solution_Programmers_59413_입양시각구하기.java
+++
@@ -1,5 +0,0 @@
-package Apr_2022.week2;
-
-public class Solution_Programmers_59413_입양시각구하기 {
-
-}
---
+++ src/Apr_2022/week3/Solution_Programmers_59413_입양시각구하기.java
@@ -0,0 +1,11 @@
+package Apr_2022.week3;
+
+public class Solution_Programmers_59413_입양시각구하기 {
+	/*
+	 SET @hour := -1;
+	  
+	 SELECT @hour := @hour+1 as 'HOUR', (SELECT(COUNT(*) FROM ANIMAL_OUTS WHERE @hour=HOUR(DATETIME)) as 'COUNT'
+	 FROM ANIMAL_OUTS
+	 WHERE @hour < 23;
+	 */
+}
