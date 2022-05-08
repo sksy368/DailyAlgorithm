88387b3a9f3e64b5d7240da45c6fdfae9b05f0cd
roseline <sksy368@naver.com>
2022-05-09 00:08:28 +0900

    Chore: Change class name

--- src/May_2022/week1/Recruitment_2022_썸머코딩여름방학스타트업인턴프로그램_1.java
+++ src/May_2022/week1/Recruitment_2022_썸머코딩여름방학스타트업인턴프로그램_1.java
@@ -1,6 +1,6 @@
 package May_2022.week1;
 
-public class Recruitment_2022_썸머코딩스타트업인턴프로그램 {
+public class Recruitment_2022_썸머코딩여름방학스타트업인턴프로그램_1 {
 	public int solution(int[][] atmos) {
 
         int answer = 0;
@@ -15,7 +15,7 @@ public class Recruitment_2022_썸머코딩스타트업인턴프로그램 {
 				day = 0;
 			}
 
-			else if(atmos[i][0] >= 81 || atmos[i][1] >= 36) { //  나쁨일때
+			else if(atmos[i][0] >= 81 || atmos[i][1] >= 36) { //  나쁨 일 때
                 if(day > 2 || day == 0) {
                     answer++;
                     day = 0;
@@ -23,11 +23,10 @@ public class Recruitment_2022_썸머코딩스타트업인턴프로그램 {
 				day++;
 			}
 			
-            else if(day != 0) day++;
+            else if(day != 0) day++; // 마스크를 착용하지 않더라도 착용 시간 갱신
             
 		}
 
 		return answer;
-
     }
 }
