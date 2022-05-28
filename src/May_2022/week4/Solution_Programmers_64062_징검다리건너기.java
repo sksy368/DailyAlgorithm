1f1289f1593052ef1b22ffa9a09a23c3a9a0fce0
roseline <sksy368@naver.com>
2022-05-28 17:41:37 +0900

    Code: Unsolve due to timeout

---
+++ src/May_2022/week4/Solution_Programmers_64062_징검다리건너기.java
@@ -0,0 +1,29 @@
+package May_2022.week4;
+
+public class Solution_Programmers_64062_징검다리건너기 {
+
+	class Solution {
+	    public int solution(int[] stones, int k) {
+	        int zeroCnt = 0; // 0인 디딤돌 개수
+	        int answer = 0; // 징검다리를 건넌 니니즈 친구들 수
+	        
+	        loop: while(true) {
+	        	for(int i = 0; i < stones.length; i++) {
+	                if(stones[i] == 0) zeroCnt++;
+	                else {
+	                    stones[i]--;
+	                    zeroCnt = 0;
+	                }
+	                
+	                if(zeroCnt >= k) break loop;
+	            }
+	            
+	            
+	            zeroCnt = 0;
+	            answer++;
+	        }
+	        
+	        return answer;
+	    }
+	}
+}
